ab0a4428f50a5a22b7294fcc7d979dd86350c2b0
Viktor Popp <ViktorPopp.dev@gmail.com>
2024-08-09 17:46:01 +0200

    Finally added precompiled headers

---
+++ HexuroEngine/pch.h
@@ -0,0 +1,49 @@
+#ifndef _HEXURO_PCH_
+#define _HEXURO_PCH_
+
+// Input/Output
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+// Strings and Containers
+#include <string>
+#include <vector>
+#include <list>
+#include <deque>
+#include <forward_list>
+#include <array>
+#include <bitset>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+
+// Memory Management
+#include <memory>
+#include <shared_mutex>
+
+// Algorithms
+#include <algorithm>
+#include <numeric>
+#include <execution>
+
+// Assertions and Error Handling
+#include <cassert>
+#include <exception>
+#include <stdexcept>
+
+// Character and Math
+#include <cctype>
+#include <cmath>
+#include <complex>
+
+// Time and Concurrency
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <future>
+#include <atomic>
+
+#endif // _HEXURO_PCH_
