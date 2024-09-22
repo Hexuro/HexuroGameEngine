2944e67e029cf0cfd4d6ba0d199db4eae59e8e78
Viktor Popp <viktorpopp.dev@gmail.com>
2024-09-22 13:24:08 +0200

    Tried creating Camera class again
    Hop it will work this time

--- HexuroEngine/Renderer/OpenGL/Camera.cpp
+++ HexuroEngine/Renderer/OpenGL/Camera.cpp
@@ -2,19 +2,20 @@
 #include "Camera.h"
 
 namespace Hexuro {
-    Camera::Camera(int window_width, int window_height, glm::vec3 initialPosition)
+    Camera::Camera(uint32_t width, uint32_t height, glm::vec3 pos)
     {
-        windowHeight = window_height;
-        windowWidth = window_width;
-        position = initialPosition;
+        m_WindowWidth = width;
+        m_WindowHeight = height;
+        position = pos;
     }
-    void Camera::Matrix(float FOVdeg, float nearPlane, float farPlane, Shader& shader, const char* uniform, const Window& window)
+
+    void Camera::Matrix(float FOVdeg, float nearPlane, float farPlane, Shader& shader, const char* uniform)
     {
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
 
         view = glm::lookAt(position, position + orientation, up);
-        projection = glm::perspective(glm::radians(FOVdeg), (float)window.GetWidth() / window.GetHeight(), nearPlane, farPlane);
+        projection = glm::perspective(glm::radians(FOVdeg), (float)m_WindowWidth / m_WindowHeight, nearPlane, farPlane);
 
         glUniformMatrix4fv(glGetUniformLocation(shader.ID, uniform), 1, GL_FALSE, glm::value_ptr(projection * view));
     }
--- HexuroEngine/Renderer/OpenGL/Camera.h
+++ HexuroEngine/Renderer/OpenGL/Camera.h
@@ -8,31 +8,28 @@
 #include<glm/gtc/type_ptr.hpp>
 #include<glm/gtx/rotate_vector.hpp>
 #include<glm/gtx/vector_angle.hpp>
-#include"Shader.h"
-#include "Renderer/Window.h"
+#include <stdint.h>
+#include "Shader.h"
 
 namespace Hexuro {
     class Camera {
     public:
+        Camera(uint32_t width, uint32_t height, glm::vec3 pos);
+
+        // Updates and exports the camera matrix to the Vertex Shader
+        void Matrix(float FOVdeg, float nearPlane, float farPlane, Shader& shader, const char* uniform);
+        void Inputs(GLFWwindow* window);
+
         glm::vec3 position;
         glm::vec3 orientation = glm::vec3(0.0f, 0.0f, -1.0f);
         glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
 
-        // Prevents the camera from jumping around when first clicking left click
-        bool firstClick = true;
-
-        uint32_t windowWidth;
-        uint32_t windowHeight;
-
         float speed = 0.1f;
         float sensitivity = 100.0f;
 
-        Camera(int window_width, int window_height, glm::vec3 position);
-
-        // Updates and exports the camera matrix to the Vertex Shader
-        void Matrix(float FOVdeg, float nearPlane, float farPlane, Shader& shader, const char* uniform, const Window& window);
-        // Handles camera inputs
-        void Inputs(GLFWwindow* window);
+    private:
+        uint32_t m_WindowWidth;
+        uint32_t m_WindowHeight;
     };
 
 }
